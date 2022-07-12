67541c5d982c0860d13b3c5849b3f100f1599884
shijinyu <sjym90@gmail.com>
2022-07-12 23:37:01 +0800

    fix: field remove and edit method

--- src/generated/public.ts
+++ src/generated/public.ts
@@ -190,3 +190,36 @@ export function useUpdateDeviceMutation(baseOptions?: Apollo.MutationHookOptions
 export type UpdateDeviceMutationHookResult = ReturnType<typeof useUpdateDeviceMutation>;
 export type UpdateDeviceMutationResult = Apollo.MutationResult<Types.UpdateDeviceMutation>;
 export type UpdateDeviceMutationOptions = Apollo.BaseMutationOptions<Types.UpdateDeviceMutation, Types.UpdateDeviceMutationVariables>;
+export const DeleteDeviceDocument = gql`
+    mutation deleteDevice($id: uuid!) {
+  delete_device_by_pk(id: $id) {
+    deviceName
+  }
+}
+    `;
+export type DeleteDeviceMutationFn = Apollo.MutationFunction<Types.DeleteDeviceMutation, Types.DeleteDeviceMutationVariables>;
+
+/**
+ * __useDeleteDeviceMutation__
+ *
+ * To run a mutation, you first call `useDeleteDeviceMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useDeleteDeviceMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [deleteDeviceMutation, { data, loading, error }] = useDeleteDeviceMutation({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useDeleteDeviceMutation(baseOptions?: Apollo.MutationHookOptions<Types.DeleteDeviceMutation, Types.DeleteDeviceMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<Types.DeleteDeviceMutation, Types.DeleteDeviceMutationVariables>(DeleteDeviceDocument, options);
+      }
+export type DeleteDeviceMutationHookResult = ReturnType<typeof useDeleteDeviceMutation>;
+export type DeleteDeviceMutationResult = Apollo.MutationResult<Types.DeleteDeviceMutation>;
+export type DeleteDeviceMutationOptions = Apollo.BaseMutationOptions<Types.DeleteDeviceMutation, Types.DeleteDeviceMutationVariables>;
